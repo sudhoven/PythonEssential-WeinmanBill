a5dc6be69b8dd1fe05284f7284f20a8a9b7dd9ad
baky laky <baky@laky.com>
2020-05-16 03:26:27 +0200

    Imported script helloVersion

---
+++ Exercise Files/Chap01/helloVersion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+import platform
+
+
+    
+def main():
+    message()
+def message():
+    print ('The value of __name__ is ' + __name__)
+
+
+if __name__ == '__main__': main()
